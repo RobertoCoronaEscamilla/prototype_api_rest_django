8663e1adec5d499821a1780da744f5d2695620f1
Roberto Corona <rober.corona@gmail.com>
2021-09-03 06:46:52 +0200

    first commit

---
+++ myapi/models.py
@@ -0,0 +1,7 @@
+# models.py
+from django.db import models
+class Person(models.Model):
+    name = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
